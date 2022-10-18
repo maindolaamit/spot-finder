d9601d0e268b2cef8379037d130226b345e80f50
Amit Maindola <maindola.amit@gmail.com>
2022-10-18 08:21:46 +0530

    added location card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import SearchAppBar from "./components/app-menu/app-menu";
-import MultiActionAreaCard from "./components/address-card/address-card";
+import LocationAreaCard from "./components/address-card/address-card";
+import LocationData from "./data/location-data";
 
 function App() {
+  const location = LocationData[1];
   return (
     <div className="App">
       <SearchAppBar/>
-      <MultiActionAreaCard/>
+      <LocationAreaCard image_url={location.image_url} address={location.address}
+                        description={location.description} name={location.name}/>
     </div>
   );
 }
--- src/components/address-card/address-card.jsx
+++ src/components/address-card/address-card.jsx
@@ -3,31 +3,31 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import {Button, CardActionArea, CardActions} from '@mui/material';
 
-export default function MultiActionAreaCard() {
+export default function LocationAreaCard(props) {
+  const {image_url, name, description} = props
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{maxWidth: 345}}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
+          image={image_url}
+          alt={name}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {description}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
-          Share
+          View
         </Button>
       </CardActions>
     </Card>
